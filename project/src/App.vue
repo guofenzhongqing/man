cbf5931d2dbe21a3b8ef1a2878f81f77d0279452
DESKTOP-9J7QVV9\力诺科技 <405232275@qq.com>
2019-03-26 19:50:16 +0800

    452

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,22 +1,37 @@
 <template>
   <div id="app">
-      <!--<MyHeader></MyHeader>-->
       <!--<AllCity></AllCity>-->
-      <Head></Head>
+      <!--<Head></Head>-->
+      <!--<MyOrder></MyOrder>-->
+      <!--<IntegralMall></IntegralMall>-->
+      <!--<VipCard></VipCard>-->
+      <!--<Serve></Serve>-->
+      <!--<DownLoad></DownLoad>-->
+       <hot1></hot1>
       <router-view></router-view>
   </div>
 </template>
 
 <script>
   import AllCity from "./components/AllCity";
-  import MyHeader from "./components/MyHeader";
   import Head from "./components/mine/Head";
+  import MyOrder from "./components/mine/MyOrder";
+  import IntegralMall from "./components/mine/IntegralMall";
+  import VipCard from "./components/mine/VipCard";
+  import Serve from "./components/mine/Serve";
+  import DownLoad from "./components/mine/DownLoad";
+  import hot1 from "./components/mine/hot/hot1";
 export default {
   name: 'App',
   components: {
     AllCity,
-    MyHeader,
-    Head
+    Head,
+    MyOrder,
+    IntegralMall,
+    VipCard,
+    Serve,
+    DownLoad,
+    hot1
   }
 }
 </script>
@@ -27,5 +42,13 @@ export default {
     list-style-type: none;
     margin: 0;
     padding: 0;
+  }
+  body,html{
+    width: 100%;
+    height: 100%;
+  }
+  #app{
+    width: 100%;
+    height: 100%;
   }
 </style>
