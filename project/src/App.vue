15d07cfd14663c83b1198ca062660539727a51d7
guofenzhongqing <48786919+guofenzhongqing@users.noreply.github.com>
2019-03-26 19:58:15 +0800

    Add files via upload

---
+++ project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+@import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+  *{
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    outline:none;
+    border:none
+  }
+  html, body {
+    width: 100%;
+    height: 100%;
+  }
+  #app {
+    width: 100%;
+    height: 100%;
+  }
+</style>
