74b3742092dddc72dcd0c9583fc82fdabac94315
SC-201811011602\lanou <1294923276@qq.com>
2019-03-28 09:30:36 +0800

    sdfglkl

--- project/src/main.js
+++ project/src/main.js
@@ -3,24 +3,31 @@ import App from './App'
 import router from './router'
 import {} from './assets/js/resize'
 import './assets/css/reset.css'
+import './assets/css/Iconfont.css'
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import vuex from 'vuex'
 Vue.use(vuex);
 Vue.use(MintUI);
-
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 
 const store = new vuex.Store({
   state: {
-    array: []
+    arr: [],
+    obj: {}
   },
   getters: {},
   mutations: {
     history(state, payload) {
-     state.array.push(payload);
-     localStorage.setItem("arr", JSON.stringify(state.array));
+      state.arr.push(payload);
+      localStorage.setItem("history", JSON.stringify(state.arr));
+    },
+    toCity(state, payload) {
+     localStorage.setItem("city", JSON.stringify(payload));
     }
   },
   actions: {},
@@ -29,7 +36,7 @@ const store = new vuex.Store({
 
 new Vue({
   el: '#app',
-  router, store,
+  router, store,render: h => h(App),
   components: { App },
   template: '<App/>'
 })
